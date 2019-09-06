033ad025b1e2b60f1f5b3ffa2251008c2f73c80b
Larry Wright <psyconerd@gmail.com>
2019-09-05 20:10:56 -0400

    first steps

---
+++ nnPlay/classes/Activations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nnPlay
+{
+    public static class Activations
+    {
+        public static double Sigmoid(double inVal)
+        {
+            return 1 / (1 + Math.Exp(-inVal));
+        }
+    }
+}
---
+++ nnPlay/classes/Neuron.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nnPlay
+{
+    class Neuron
+    {
+        double[] inputs;
+        double[] weights;
+        double returnVal;
+        public double Output()
+        {
+            if (inputs.Length != weights.Length) return 0.0;
+            returnVal = 0.0;
+            for (int i = 0; i< inputs.Length; i++)
+            {
+                returnVal += inputs[i] * weights[i];
+            }
+            returnVal = Activations.Sigmoid(returnVal);
+            return returnVal;
+        }
+    }
+}
