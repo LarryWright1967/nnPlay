9972f389c6bfdee4f56d446a5cbc25bee83dd254
Larry Wright <psyconerd@gmail.com>
2019-09-18 06:33:36 -0400

    nothing, fell asleep

--- nnPlay/classes/Activations.cs
+++ nnPlay/classes/Activations.cs
@@ -6,6 +6,15 @@ using System.Threading.Tasks;
 
 namespace nnPlay
 {
+    https://youtu.be/ILsA4nyG7I0
+
+    //public static SIGMOID: ActivationFunction = {
+    //output: x => 1 / (1 + Math.exp(-x)),
+    //der: x => {
+    //let output = Activations.SIGMOID.output(x);
+    //return output * (1 - output);
+    //}
+    //};
     public static class Activations
     {
         public static double Sigmoid(double inVal)
